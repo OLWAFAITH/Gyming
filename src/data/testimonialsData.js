123936ca23d8b11ae40eab764bdca79beb101969
Oluwa Faith <alukofaithtemitope7@gmail.com>
2022-08-22 15:21:08 +0100

    gyming system

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.jpg";
+import image2 from "../assets/t-image2.jpeg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by building this exclusive `Gyming System` ",
+    name: 'Aluko Faith Temitope',
+    status : 'C.E.O'
+  },
+  {
+    image: image2,
+    review: 'Its been a Good Time Coaching Here Join Us And Know More About Us ',
+    name: 'Oluwaseyi Ishola',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
+    name: 'FRANKLIN',
+    status: "CUSTOMER"
+  }
+];
